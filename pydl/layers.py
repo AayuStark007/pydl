07f5bfeb65fb58621b823ce4aaa1562c5ea54bb0
Aayush Gupta <aayu.gupta.96@gmail.com>
2018-12-28 14:28:24 +0530

    Add activation: tanh

--- pydl/layers.py
+++ pydl/layers.py
@@ -2,7 +2,7 @@
 Define and configure the layers used in a neural network.
 Each layer passes the inputs forward and propagates the gradient backwards.
 """
-from typing import Dict
+from typing import Dict, Callable
 import numpy as np
 
 from pydl.tensor import Tensor
@@ -57,3 +57,36 @@ class Linear(Layer):
         self.grads["b"] = np.sum(grad, axis=0)
         self.grads["w"] = self.inputs.T @ grad
         return grad @ self.params["w"].T
+
+F = Callable[[Tensor], Tensor]
+
+class Activation(Layer):
+    """
+    An activation layer applies a fn elementwise to its inputs.
+    """
+    def __init__(self, f: F, f_prime: F) -> None:
+        super().__init__()
+        self.f = f
+        self.f_prime = f_prime
+
+    def forward(self, inputs: Tensor) -> Tensor:
+        self.inputs = inputs
+        return self.f(inputs)
+    
+    def backward(self, grad: Tensor) -> Tensor:
+        """
+        if y = f(x) and x = g(z)
+        then dy/dz = f'(x) * g'(z)
+        """
+        return self.f_prime(self.inputs) * grad
+
+def tanh(x: Tensor) -> Tensor:
+    return np.tanh(x)
+
+def tanh_prime(x: Tensor) -> Tensor:
+    y = tanh(x) 
+    return 1 - y ** 2
+
+class Tanh(Activation):
+    def __init__(self):
+        super().__init__(self, tanh, tanh_prime)
